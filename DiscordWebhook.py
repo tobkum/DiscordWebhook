8f0baa390c825a08146c73bb1e26dbf974731198
tobi <tobiaskummer@googlemail.com>
2025-03-21 04:36:33 +0100

    refactor

--- DiscordWebhook.py
+++ DiscordWebhook.py
@@ -33,110 +33,49 @@ class DiscordWebhookEventListener(DeadlineEvents.DeadlineEventListener):
         del self.OnJobErrorCallback
 
     def OnJobError(self, job, error, *args):
-        discord_webhook_url = self.GetConfigEntry("DiscordWebhookURL")
-        job_name = job.JobName
-        user_name = job.JobUserName
-
-        full_message = f":exclamation: Job error. **Error:** {error} **Job:** `{job_name}` **User:** `{user_name}`"
-
-        data = {"content": full_message}
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(
-            discord_webhook_url, data=json.dumps(data), headers=headers
-        )
-        if response.status_code != 204:
-            self.LogWarning(
-                "Failed to send Discord message. Status code: "
-                + str(response.status_code)
-            )
+        message = ":exclamation: Job error. **Error:** {error}"
+        self.send_discord_message(job, message)
 
     def OnJobStarted(self, job):
-        user_name = job.JobUserName
-        self.send_discord_message(
-            job, f":arrow_forward: Job started. **User:** `{user_name}`"
-        )
+        message = ":arrow_forward: Job started."
+        self.send_discord_message(job, message)
 
     def OnJobSuspended(self, job):
-        user_name = job.JobUserName
-        self.send_discord_message(
-            job, f":pause_button: Job suspended. **User:** `{user_name}`"
-        )
+        message = ":pause_button: Job suspended."
+        self.send_discord_message(job, message)
 
     def OnJobResumed(self, job):
-        user_name = job.JobUserName
-        self.send_discord_message(
-            job, f":arrow_forward: Job resumed. **User:** `{user_name}`"
-        )
+        message = ":arrow_forward: Job resumed."
+        self.send_discord_message(job, message)
 
     def OnJobSubmitted(self, job):
-        discord_webhook_url = self.GetConfigEntry("DiscordWebhookURL")
-        job_name = job.JobName
-        user_name = job.JobUserName
-
-        full_message = (
-            f":rocket: Job submitted. **Job:** `{job_name}` **User:** `{user_name}`"
-        )
-
-        data = {"content": full_message}
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(
-            discord_webhook_url, data=json.dumps(data), headers=headers
-        )
-        if response.status_code != 204:
-            self.LogWarning(
-                "Failed to send Discord message. Status code: "
-                + str(response.status_code)
-            )
+        message = ":rocket: Job submitted."
+        self.send_discord_message(job, message)
 
     def OnJobFinished(self, job):
-        discord_webhook_url = self.GetConfigEntry("DiscordWebhookURL")
-        job_name = job.JobName
-        user_name = job.JobUserName
         start_time = job.JobStartedDateTime
         end_time = job.JobCompletedDateTime
         duration = end_time - start_time
 
         if len(job.JobOutputDirectories) > 0:
             output_path = job.JobOutputDirectories
-            full_message = f":white_check_mark: Job finished. **Job:** `{job_name}` **User:** `{user_name}` **Duration:** `{duration}` **Output path:** `{output_path[0]}`"
+            message = f":white_check_mark: Job finished. **Duration:** `{duration}` **Output path:** `{output_path[0]}`"
         else:
             output_path = "Unknown"
-            full_message = f":white_check_mark: Job finished. **Job:** `{job_name}` **User:** `{user_name}` **Duration:** `{duration}` **Output path:** `{output_path}`"
-
-        data = {"content": full_message}
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(
-            discord_webhook_url, data=json.dumps(data), headers=headers
-        )
-        if response.status_code != 204:
-            self.LogWarning(
-                "Failed to send Discord message. Status code: "
-                + str(response.status_code)
-            )
+            message = f":white_check_mark: Job finished. **Duration:** `{duration}` **Output path:** `{output_path}`"
+        self.send_discord_message(job, message)
 
     def OnJobFailed(self, job):
-        discord_webhook_url = self.GetConfigEntry("DiscordWebhookURL")
-        job_name = job.JobName
-        user_name = job.JobUserName
-
-        full_message = f":x: Job failed. **Job:** `{job_name}` **User:** `{user_name}`"
-
-        data = {"content": full_message}
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(
-            discord_webhook_url, data=json.dumps(data), headers=headers
-        )
-        if response.status_code != 204:
-            self.LogWarning(
-                "Failed to send Discord message. Status code: "
-                + str(response.status_code)
-            )
+        message = ":x: Job failed."
+        self.send_discord_message(job, message)
 
     def send_discord_message(self, job, message):
         discord_webhook_url = self.GetConfigEntry("DiscordWebhookURL")
-        job_name = job.JobName
 
-        full_message = f"{message} **Job:** `{job_name}`"
+        job_name = job.JobName
+        user_name = job.JobUserName
+
+        full_message = f"{message} **Job:** `{job_name}` **User:** `{user_name}`"
 
         data = {"content": full_message}
         headers = {"Content-Type": "application/json"}
